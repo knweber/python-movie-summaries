754b064215f03607e3a4433454fabd1c25f60030
Kris <kris.weber.1994@gmail.com>
2017-11-29 13:34:26 -0800

    Add initial methods to handle API call

--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
 import http.client
 import os
+import urllib.request
+import urllib.parse
 OMDB_KEY = os.environ['OMDB_KEY']
 
 # format given title
 
 def format_title(title):
-
+    converted = urllib.parse.urlencode({'t': title})
+    return converted
 
 # set-up API client
 
@@ -14,14 +17,16 @@ def establish_connection(title):
     request_plot(conn, title)
 
 def request_plot(connection, title):
-    connection.request("GET", "/?apikey=" + OMDB_KEY + "&t=" + title + "&plot=full")
+    connection.request("GET", "/?apikey=" + OMDB_KEY + "&" + title + "&plot=full")
     handle_response()
 
 def handle_response(connection):
     res = connection.getresponse()
     data = res.read()
-    plot = data["Plot"]
-    print plot
+    if data["Plot"]:
+        return data["Plot"]
+    else:
+        return "Please check your spelling."
 
 def main():
     raw_title = input("Enter a title: ")
